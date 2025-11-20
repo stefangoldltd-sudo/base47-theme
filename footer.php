18f1e5a4203a9b561ad0a4187b836c89b1976edd
stefangoldltd-sudo <stefangoldltd@gmail.com>
2025-11-20 10:22:34 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,23 +1,6 @@
 <?php
-/**
- * Footer template for Base47 Theme
- */
-
-if ( ! defined( 'ABSPATH' ) ) {
-    exit;
-}
+if (!defined('ABSPATH')) exit;
+wp_footer();
 ?>
-        </div><!-- .content-wrapper -->
-    </main><!-- .site-main -->
-
-    <footer class="site-footer">
-        <div class="content-wrapper">
-            <small>&copy; <?php echo date( 'Y' ); ?> Studio47. All rights reserved.</small>
-        </div>
-    </footer>
-
-</div><!-- #page -->
-
-<?php wp_footer(); ?>
 </body>
 </html>
